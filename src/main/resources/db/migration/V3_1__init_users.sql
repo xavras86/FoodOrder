a3c31b38875680a59e245b6a916b3ee98606cbad
Marcin Sikora <sikomar@interia.pl>
2023-08-18 17:34:34 +0200

    22. init data edit2

--- src/main/resources/db/migration/V3_1__init_users.sql
+++ src/main/resources/db/migration/V3_1__init_users.sql
@@ -6,7 +6,7 @@ ALTER TABLE customer
 ADD COLUMN user_id INT,
 ADD FOREIGN KEY (user_id) REFERENCES food_ordering_user (user_id);
 
-insert into food_ordering_user (user_name, email, password, active) values ('ustomer', 'customer@gmail.com', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true);
+insert into food_ordering_user (user_name, email, password, active) values ('customer', 'customer@gmail.com', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true);
 insert into food_ordering_user (user_name, email, password, active) values ('owner', 'owner@gmail.com', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true);
 
 insert into food_ordering_role (role_id, role) values (1, 'CUSTOMER'), (2, 'OWNER');
